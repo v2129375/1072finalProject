cc8173d84f5019d6d2d03b968c63fa01d8ac1fec
v2129375 <randomjerry@foxmail.com>
2019-05-14 22:13:03 +0800

    0.02

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
 import java.util.*;
 public class Main {
     public static void main(String arg[]){
-        System.out.println("Hello");
+        System.out.println("請選擇你使用的資料結構");
+        System.out.println("1、平衡二叉樹  2、紅黑樹  3、跳躍串列");
+        Scanner sc =new Scanner(System.in);
+        int choose=sc.nextInt();
+
+        if(choose==1){
+
+        }else if(choose==2){
+
+        }else if(choose==3){
+
+        }
     }
 }
