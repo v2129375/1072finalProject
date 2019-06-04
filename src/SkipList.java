f55424dfea92cfc47932dc1c6d08fd819759a96c
v2129375 <randomjerry@foxmail.com>
2019-06-04 23:04:58 +0800

    1

--- src/SkipList.java
+++ src/SkipList.java
@@ -1,8 +1,8 @@
 public class SkipList {
-    Listnode first;//最上方第一個節點指針
-    Listnode down;//尾節點指針
-    Listnode right;//右節點指針
-    Listnode left;//左節點指針
+    Listnode first=new Listnode();//最上方第一個節點指針
+    Listnode down=new Listnode();//尾節點指針
+    Listnode right=new Listnode();//右節點指針
+    Listnode left=new Listnode();//左節點指針
     int level;
     SkipList(int input[]){
         int size=input.length;
@@ -19,8 +19,13 @@ public class SkipList {
                 System.out.println("重複的節點");//遇到重複節點不添加，回傳錯誤信息
             }else{
                 Listnode temp=first;
-                while(in<temp.right.data&&temp.right.data!=0){
-                    temp.right=temp.right.right;
+                while(true){
+                    if(in<temp.right.data&&temp.right.data!=0){
+                        temp.right=temp.right.right;//找到同層中與加入的值最接近且小於其的點
+                    }else if(temp.right.data==0){
+
+                    }
+
                 }
             }
         }
